4b4f52989d0832d05bae080bbf7f9d90f379322b
mohammadrezachegini <mohammadreza.chegini01@gmail.com>
2023-02-28 11:06:14 -0800

    create mongodb connection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const{
     NotFoundError
 } = require("./util/errorHandler")
 const app = express()
+require("./config/mongo.config")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.get("/", (req,res,next) => {
---
+++ config/mongo.config.js
@@ -0,0 +1,9 @@
+const {default: mongoose} = require("mongoose")
+
+const DB_URL = "mongodb://127.0.0.1:27017/blog";
+
+mongoose.connect(DB_URL).then(()=>{
+    console.log("connection is successfully");
+}).catch((e)=>{
+    console.log("no connection " + e);
+});
