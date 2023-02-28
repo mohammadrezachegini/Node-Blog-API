d578559b1f14c0f15a6f15f3b6bd35679e3d278d
mohammadrezachegini <mohammadreza.chegini01@gmail.com>
2023-02-28 12:02:54 -0800

    put validation on schema

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,12 +1,18 @@
 const {BlogModel} = require("../model/blog.model")
 async function createBlog (req,res,next) {
-    const {title, text} = req.body;
-    const result = await BlogModel.create({
-        text,
-        title
-    })
+    try {
+        const {title, text} = req.body;
+        const result = await BlogModel.create({
+            text,
+            title
+        })
 
-    res.send(result)
+        res.send(result)
+    }
+    catch (err){
+        console.log(err)
+        next(err)
+    }
 }
 
 module.exports ={
--- model/blog.model.js
+++ model/blog.model.js
@@ -1,8 +1,8 @@
 const {Schema, model} = require("mongoose")
 
 const BlogSchema = new Schema({
-    title: {type: String, required: true},
-    text: {type: String, required: true},
+    title: {type: String, required: true, trim: true, minLength:5},
+    text: {type: String, required: true, trim: true, minLength: 5},
     show: {type: Boolean, default: false},
     likes: {type: Number, default: 0},
     bookmarks: {type: [String], default: []},
